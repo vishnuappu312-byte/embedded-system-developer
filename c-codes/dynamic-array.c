61a215bce9448cd0fa349aa68c91a07c676ea7ea
vishnuappu312-byte <vishnuappu312@gmail.com>
2026-03-11 20:47:32 +0530

    Fix dynamic array issue

--- c-codes/dynamic-array.c
+++ c-codes/dynamic-array.c
@@ -4,13 +4,12 @@
 int main()
 {
     int n, i;
-    int old_n;
     int *arr;
 
     printf("Enter number of elements: ");
     scanf("%d", &n);
 
-    arr = malloc(n * sizeof(int));
+    arr = (int *)malloc(n * sizeof(int));
 
     if(arr == NULL)
     {
@@ -20,42 +19,51 @@ int main()
 
     printf("Enter %d numbers:\n", n);
     for(i = 0; i < n; i++)
+    {
         scanf("%d", &arr[i]);
+    }
 
     printf("Array elements are:\n");
     for(i = 0; i < n; i++)
+    {
         printf("%d ", arr[i]);
+    }
 
-    old_n = n;
+    
+//free(arr);
 
-    printf("\nEnter new number of elements: ");
+    printf("Enter new number of elements: ");
     scanf("%d", &n);
+int *temp = realloc(arr, (n) * sizeof(int));
+if(temp == NULL)
+{
+    printf("Memory reallocation failed\n");
+    free(arr); 
+    return 1;
+}
+arr = temp; 
 
-    int *temp = realloc(arr, n * sizeof(int));
-    if(temp == NULL)
-    {
-        printf("Memory reallocation failed\n");
-        free(arr);
-        return 1;
-    }
-
-    arr = temp;
-
-    if(n > old_n)
-    {
-        printf("Enter %d more numbers:\n", n - old_n);
-        for(i = old_n; i < n; i++)
-            scanf("%d", &arr[i]);
-    }
-
-    printf("Updated array elements are:\n");
-    for(i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-
+for(i = 0; i < n; i++)
+{
+    scanf("%d", &arr[i]);
+}   
+printf("Updated array elements are:\n");
+for(i = 0; i < n; i++)
+{
+    printf("%d ", arr[i]);
+}   
     free(arr);
-    arr = NULL;
 
-    printf("\nMemory freed successfully\n");
+printf("after free array elements are:\n");
+for(i = 0; i < n; i++)
+{
+    printf("%d ", arr[i]);
+}   
+
+
+
+
+
 
     return 0;
 }
