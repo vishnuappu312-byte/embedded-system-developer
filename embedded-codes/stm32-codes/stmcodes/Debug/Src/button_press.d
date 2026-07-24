cb01bb2ecd7a84624c6081ece3573714ef424fc4
vishnuappu312-byte <vishnuappu312@gmail.com>
2026-07-24 15:05:10 +0530

    Remove contents of stm32-codes and keep directory

--- embedded-codes/stm32-codes/stmcodes/Debug/Src/button_press.d
+++
@@ -1,7 +0,0 @@
-Src/button_press.o: ../Src/button_press.c \
- /Users/vishnu/Desktop/personalcodes/stmcodes/driver/inc/stm32f401xx_gpio.h \
- /Users/vishnu/Desktop/personalcodes/stmcodes/driver/inc/stm32f401xx.h \
- ../Inc/delay.h
-/Users/vishnu/Desktop/personalcodes/stmcodes/driver/inc/stm32f401xx_gpio.h:
-/Users/vishnu/Desktop/personalcodes/stmcodes/driver/inc/stm32f401xx.h:
-../Inc/delay.h:
